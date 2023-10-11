ced705c7dbedc76cd4357e9c9438bc03925b2bfb
DanielAgusSatrio <114908515+DanielAgusSatrio@users.noreply.github.com>
2023-10-11 23:43:49 +0700

    Add files via upload

---
+++ DemoNilai.java
@@ -0,0 +1,18 @@
+package tugaspbo1;
+
+public class DemoNilai {
+
+    public static void main(String[] args) {
+            Nilai nilaiAkhir = new Nilai();
+
+            nilaiAkhir.nim = "2210631170017";
+            nilaiAkhir.nama = "Daniel Agus Satrio";
+            nilaiAkhir.absen = 100;
+            nilaiAkhir.tugas = 85;
+            nilaiAkhir.uts = 80;
+            nilaiAkhir.uas = 90;
+
+            nilaiAkhir.CetakNilai();
+        }
+
+    }
---
+++ Mahasiswa.java
@@ -0,0 +1,17 @@
+package tugaspbo1;
+
+public class Mahasiswa {
+
+    void tidak membaca (){
+        System.out.println("Mahasiswa yang tidak suka membaca");
+    }
+    
+    void menyontek (){
+        System.out.println("Mahasiswa yang suka menyontek");
+    }
+    
+    void memodifikasi (){
+        System.out.println("Mahasiswa yang pandai memodifikasi");
+    }
+    
+}
